56de9b66cc19c55be9b1f6bf2c72667772adbef6
Nick Chen <chenyahua2012@hotmail.com>
2020-05-17 00:10:28 -0400

    buildup a basic react node runtime environment

---
+++ client/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+
+ReactDOM.render( <Home/>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
